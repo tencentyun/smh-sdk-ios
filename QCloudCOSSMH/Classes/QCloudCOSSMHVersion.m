e8f925cbfc2702080347d73db5310bea2e7316ab
garenwang <garenwang@tencent.com>
2025-02-18 21:58:14 +0800

    fix  sdk  version

--- QCloudCOSSMH/Classes/QCloudCOSSMHVersion.m
+++ QCloudCOSSMH/Classes/QCloudCOSSMHVersion.m
@@ -5,6 +5,6 @@
 //  Created by garenwang on 2021/10/11.
 //
 #import "QCloudCOSSMHVersion.h"
-NSString *const QCloudCOSSMHModuleVersion = @"1.0.1";
+NSString *const QCloudCOSSMHModuleVersion = @"1.0.2";
 NSString *const QCloudCOSSMHModuleName = @"QCloudCOSSMH";
 
