768e10e637fd92c797807c5d86136f5201b00683
garenwang <garenwang@tencent.com>
2025-03-20 18:00:29 +0800

    fix

--- QCloudCOSSMH/Classes/Api/Request/Batch/QCloudSMHRestoreObjectRequest.h
+++ QCloudCOSSMH/Classes/Api/Request/Batch/QCloudSMHRestoreObjectRequest.h
@@ -14,7 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
  封装了QCloudSMHBatchRestoreRecycleObjectReqeust 接口
  若返回结果为202 任务正在进行中，则轮询任务结果直到 200。
  */
-@interface QCloudSMHRestoreObjectRequest : QCloudSMHBatchBaseRequest <NSString *>
+@interface QCloudSMHRestoreObjectRequest : QCloudSMHBatchBaseRequest <NSNumber *>
 
 @end
 
